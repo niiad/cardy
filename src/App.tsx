11fc8ae371d8ede0d1d1517cbd36813158c54a6e
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2022-12-02 16:50:45 +0000

    styling and responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [cards, setCards] = useState(
+      [
+          {
+              title: `Card One`,
+              text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Amet dignissimos eaque nobis. Adipisci asperiores atque
+                        cupiditate dignissimos distinctio et, ex itaque nemo nisi
+                        nulla praesentium rerum sed similique sit tempore.`
+          },
+          {
+              title: `Card Two`,
+              text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Amet dignissimos eaque nobis. Adipisci asperiores atque
+                        cupiditate dignissimos distinctio et, ex itaque nemo nisi
+                        nulla praesentium rerum sed similique sit tempore.`
+          },
+          {
+              title: `Card Three`,
+              text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Amet dignissimos eaque nobis. Adipisci asperiores atque
+                        cupiditate dignissimos distinctio et, ex itaque nemo nisi
+                        nulla praesentium rerum sed similique sit tempore.`
+          },
+          {
+              title: `Card Four`,
+              text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Amet dignissimos eaque nobis. Adipisci asperiores atque
+                        cupiditate dignissimos distinctio et, ex itaque nemo nisi
+                        nulla praesentium rerum sed similique sit tempore.`
+          }
+      ]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <div className={"container"}>
+            <h1>Cardy</h1>
+            <div className={"cards"}>
+                {
+                    cards.map((card, index) => {
+                        return (
+                            <div key={index} className={"card"}>
+                                <h3>{card.title}</h3>
+                                <p>{card.text}</p>
+                                <button className={"button"}>Learn more</button>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        </div>
+      </section>
     </div>
   );
 }
